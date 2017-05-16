00729d7c7e0ec746e8b1346b7e7a6ac63abb9a9d
rambou <nickos.bou@gmail.com>
2017-05-16 17:12:57 +0300

    Fixes on index

--- index.php
+++ index.php
@@ -68,22 +68,26 @@ get_header(); ?>
 
                         .demo-card-square > .mdl-card__title {
                             color: #fff;
-                            background: url('https://cdn.dribbble.com/users/25514/screenshots/2016747/flat-news-app-icon-design-ramotion.gif') bottom right 15% no-repeat #46B6AC;
+                            background: url('http://www.androidguys.com/wp-content/uploads/2014/11/material_design_wallpaper-5.jpg') bottom right 15% no-repeat #46B6AC;
                         }
 
                         .disquss > .mdl-card__title {
                             background: url('https://s3.amazonaws.com/data.tackk.com/mio/37514909/14280070302105/large.gif') bottom right 15% no-repeat #46B6AC;
                         }
+
+                        .mdl-grid.center-items {
+                            justify-content: center;
+                        }
                     </style>
 
-                    <div class="mdl-grid">
+                    <div class="mdl-grid center-items">
 
                         <div class="mdl-cell mdl-cell--4-col demo-card-square mdl-card mdl-shadow--2dp">
                             <div class="mdl-card__title mdl-card--expand">
                                 <h2 class="mdl-card__title-text">Ανακοινώσεις</h2>
                             </div>
                             <div class="mdl-card__supporting-text">
-                                Ανακοινώσεις και νέα απο το ΤΕΕ...
+                                Ανακοινώσεις και νέα απο το ΤΕΕ. Ενημερωθείτε για όλες τις εξελίξεις!
                             </div>
                             <div class="mdl-card__actions mdl-card--border">
                                 <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
@@ -120,19 +124,19 @@ get_header(); ?>
                         </div>
                         <div class="mdl-card__actions mdl-card--border">
                             <ul>
-                                <li contenteditable="true">Προωθεί την πολιτική του ΤΕΕ στην Περιφέρεια</li>
-                                <li contenteditable="true">Συμβάλει στη διαμόρφωση της γενικής πολιτικής του ΤΕΕ σε
+                                <li>Προωθεί την πολιτική του ΤΕΕ στην Περιφέρεια</li>
+                                <li>Συμβάλει στη διαμόρφωση της γενικής πολιτικής του ΤΕΕ σε
                                     γενικά
                                     θέματα και μεταφέρει τον προβληματισμό και τις απόψεις της περιφέρειας στα κεντρικά
                                     όργανα.
                                 </li>
-                                <li contenteditable="true">Ασκεί την πειθαρχική διαδικασία σε Α΄ βαθμό.</li>
-                                <li contenteditable="true">Αντιμετωπίζει επί τόπου προβλήματα που έχουν σχέση με την
+                                <li>Ασκεί την πειθαρχική διαδικασία σε Α΄ βαθμό.</li>
+                                <li>Αντιμετωπίζει επί τόπου προβλήματα που έχουν σχέση με την
                                     λειτουργία
                                     του ΤΣΜΕΔΕ και ΚΥΤ.
                                 </li>
-                                <li contenteditable="true">Ασχολείται με ειδικά προβλήματα της Περιφέρειας.</li>
-                                <li contenteditable="true"> Πραγματοποιεί πραγματογνωμοσύνες σε θέματα τοπικής
+                                <li>Ασχολείται με ειδικά προβλήματα της Περιφέρειας.</li>
+                                <li> Πραγματοποιεί πραγματογνωμοσύνες σε θέματα τοπικής
                                     αρμοδιότητας με
                                     Διπλωματούχους Μηχανικούς που διορίζονται από τον Πρόεδρο του Τμήματος, Π.Δ.
                                     10/16.04.1935
@@ -144,6 +148,54 @@ get_header(); ?>
                     </div>
 
 
+                    <div class="demo-card-wide mdl-card mdl-shadow--2dp">
+                        <div class="mdl-card__title mdl-card--expand">
+                            ΠΡΟΚΗΡΥΞΕΙΣ - ΔΙΑΓΩΝΙΣΜΟΙ
+                        </div>
+                        <div class="mdl-grid">
+                            <div class="mdl-layout-spacer"></div>
+                            <div class="mdl-cell mdl-cell--12-col">
+                                <a href="http://www.pvaigaiou.gov.gr/web/guest/diagonismoi"
+                                   class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                                    ΠΕΡΙΦΕΡΕΙΑ Β. ΑΙΓΑΙΟΥ
+                                </a>
+                            </div>
+                            <div class="mdl-cell mdl-cell--4-col">
+                                <a href="http://www.mytilene.gr/category/prokiriksis/"
+                                   class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                                    ΔΗΜΟΣ ΛΕΣΒΟΥ
+                                </a>
+
+                                <div class="mdl-layout-spacer"></div>
+                                <a href="http://www.limnos.gov.gr/index.php?option=com_content&task=blogcategory&id=40&Itemid=215"
+                                   class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                                    ΔΗΜΟΣ Λημνου
+                                </a>
+                            </div>
+
+                            <div class="mdl-cell mdl-cell--4-col">
+                                <a href="http://www.chioscity.gr/dimotes/prokirikseis"
+                                   class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                                    ΔΗΜΟΣ ΧΙΟΥ
+                                </a>
+                            </div>
+
+                            <div class="mdl-cell mdl-cell--4-col">
+                                <a href="http://www.islandofsamos.gr/enhmerwsh/diagwnismoi-prokhrukseis"
+                                   class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                                    ΔΗΜΟΣ σαμου
+                                </a>
+
+                                <div class="mdl-layout-spacer"></div>
+                                <a href="http://ikaria.gov.gr/index.php/office-press/dimoprasies/erga.html"
+                                   class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                                    ΔΗΜΟΣ ικαριας
+                                </a>
+                            </div>
+
+                        </div>
+
+                    </div>
                 </div>
                 <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
 
@@ -162,8 +214,8 @@ get_header(); ?>
                             align-items: flex-start;
                         }
 
-                        .demo-card-event > .mdl-card__title > h4 {
-                            margin-top: 0;
+                        .demo-card-event > .mdl-card__title {
+                            background: url('https://cdn.dribbble.com/users/25514/screenshots/2016747/flat-news-app-icon-design-ramotion.gif');
                         }
 
                         .demo-card-event > .mdl-card__actions {
@@ -189,17 +241,29 @@ get_header(); ?>
 
                             <ul class="demo-list-item mdl-list">
                                 <li class="mdl-list__item">
-                                    <a class="mdl-color-text--white" href="http://www.michanikos.gr/">Μιχανικός</a>
+                                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-color-text--white"
+                                       href="http://www.michanikos.gr/"
+                                       target="_blank">Μιχανικός</a>
                                 </li>
-                                <li class="mdl-list__item"><a class="mdl-color-text--white" href="http://www.engoe.gr/">http://www.engoe.gr/</a>
+                                <li class="mdl-list__item"><a
+                                            class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-color-text--white"
+                                            href="http://www.engoe.gr/" target="_blank">Μηχανικοί
+                                        της γης</a>
                                 </li>
-                                <li class="mdl-list__item"><a class="mdl-color-text--white"
-                                                              href="http://www.emichanikos.gr/content.php">http://www.emichanikos.gr/content.php</a>
+                                <li class="mdl-list__item"><a
+                                            class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-color-text--white"
+                                            href="http://www.emichanikos.gr/content.php"
+                                            target="_blank">Ε-Μηχανικός</a>
                                 </li>
-                                <li class="mdl-list__item"><a class="mdl-color-text--white"
-                                                              href="http://www.iekemtee.gr/el/">http://www.iekemtee.gr/el/</a>
+                                <li class="mdl-list__item"><a
+                                            class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-color-text--white"
+                                            href="http://www.iekemtee.gr/el/" target="_blank">ΙΕΚΕΜ
+                                        ΤΕΕ</a>
                                 </li>
-                                <li class="mdl-list__item"><a class="mdl-color-text--white" href="http://web.tee.gr/">http://web.tee.gr/</a>
+                                <li class="mdl-list__item"><a
+                                            class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-color-text--white"
+                                            href="http://web.tee.gr/" target="_blank">Κεντρικό
+                                        ΤΕΕ</a>
                                 </li>
                             </ul>
 
@@ -213,6 +277,11 @@ get_header(); ?>
                     <!-- start feedwind code -->
                     <script type="text/javascript" src="https://feed.mikle.com/js/fw-loader.js"
                             data-fw-param="25207/"></script>
+                    <!-- end feedwind code -->
+
+                    <!-- start feedwind code -->
+                    <script type="text/javascript" src="https://feed.mikle.com/js/fw-loader.js"
+                            data-fw-param="26249/"></script>
                     <!-- end feedwind code -->
 
                 </div>
